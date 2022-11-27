0d6468453ef909c768c5b1db89ded8c3eed6dd85
ChangHyun Park <buschanny@gmail.com>
2022-11-27 22:55:37 +0900

    Time: 807 ms (51.50%), Space: 42.8 MB (79.64%) - LeetHub

---
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} matchsticks
+ * @return {boolean}
+ */
+var makesquare = function(matchsticks) {
+    let sum = 0;
+    for(let n of matchsticks){
+        sum += n;
+    }
+    
+    if(matchsticks.length < 4 || sum % 4 !== 0) return false;
+    
+    matchsticks.sort((a, b) => b - a);
+    
+    let checked = new Array(4).fill(0);
+    
+    return solve(0)
+    
+    function solve(x){
+        if(x === matchsticks.length){
+            return checked[0] === checked[1] && checked[1] === checked[2] && checked[2] === checked[3];
+        }
+        
+        for(let i = 0; i < 4; i++){
+            if(checked[i] + matchsticks[x] > sum / 4) continue;
+            
+            checked[i] += matchsticks[x];
+            if(solve(x + 1)) return true;
+            checked[i] -= matchsticks[x];
+        }
+        return false;
+    }
+    
+};
