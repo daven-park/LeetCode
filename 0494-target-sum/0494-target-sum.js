414e003a5ad4bf62cf4bd51f29fc3701c1be3422
ChangHyun Park <buschanny@gmail.com>
2022-12-04 15:27:13 +0900

    Time: 438 ms (62.04%), Space: 51.3 MB (26.36%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var findTargetSumWays = function(nums, target) {
+    const dp = {};
+    
+    const dfs = (i, sum) => {
+        if(i === nums.length){
+            if(sum === target) return 1;
+            return 0;
+        }
+        
+        if(!dp[i]) dp[i] = {};
+        else{
+            if (dp[i][sum] !== undefined) return dp[i][sum];
+        }
+
+        return dp[i][sum] = dfs(i + 1, sum + nums[i]) + dfs(i + 1, sum - nums[i]);
+
+    }
+    return dfs(0, 0);
+
+    
+};
