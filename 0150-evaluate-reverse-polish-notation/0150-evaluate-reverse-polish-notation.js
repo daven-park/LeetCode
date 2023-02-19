091bc696b61889cea78f317530085c9da69e9bab
ChangHyun Park <buschanny@gmail.com>
2023-02-19 22:37:45 +0900

    Time: 72 ms (85.67%), Space: 44.8 MB (62.32%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const stack = [];
+    
+
+    tokens.forEach(token => {
+        if(/^[+\-*/]$/.test(token)){
+            let sec = stack.pop();
+            let fir = stack.pop();
+            stack.push(calc(fir, sec, token));
+        }else {
+            stack.push(+token)
+        }
+    })
+    return stack.pop();
+};
+   const calc = (fir, sec, token) => {
+        switch(token) {
+            case '+' : return fir + sec;
+            case '-' : return fir - sec;
+            case '*' : return fir * sec;
+            case '/' : return fir / sec | 0;
+        }
+    }
+    
+
