88548a5cdb13f0974e2ac259004a002a6cbe3bf5
ChangHyun Park <buschanny@gmail.com>
2023-03-30 13:40:11 +0900

    Time: 61 ms (60.20%), Space: 41.9 MB (67.43%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.js
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.js
@@ -3,13 +3,14 @@
  * @return {boolean}
  */
 var hasAlternatingBits = function(n) {
+    // 이진수 변환
     let bits = n.toString(2);
-    console.log(bits)
+    
     let prev = bits[0];
+    
     for(let i = 1; i < bits.length; i++){
         if(prev === bits[i]) return false;
         prev = bits[i]
     }
     return true;
-    
 };
