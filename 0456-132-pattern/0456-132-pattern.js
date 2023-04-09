6f69c00df159f99b599401bed2be640717cdc33c
ChangHyun Park <buschanny@gmail.com>
2023-04-10 00:54:52 +0900

    Time: 85 ms (36.11%), Space: 56.3 MB (15.28%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.js
+++ 0456-132-pattern/0456-132-pattern.js
@@ -5,12 +5,13 @@
 var find132pattern = function(nums) {    
     let minN = [];
     minN[0] = nums[0];
+    
     for(let i = 1; i < nums.length; i++){
-        minN[i] = Math.min(minN[i-1],nums[i]);
+        minN[i] = Math.min(minN[i - 1],nums[i]);
     }
     
-    
     let stack = [];
+    
     for(let j = nums.length-1; j >= 0; j--){
         if(nums[j] > minN[j]){ 
             while(stack.length > 0 && stack.at(-1) <= minN[j]){
