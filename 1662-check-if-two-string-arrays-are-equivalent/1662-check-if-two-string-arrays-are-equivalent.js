916bbf98a480bd8155b4a3efd4e65de9c1791ea9
ChangHyun Park <buschanny@gmail.com>
2023-04-30 19:45:45 +0900

    Time: 58 ms (61.08%), Space: 41.5 MB (96.47%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} word1
+ * @param {string[]} word2
+ * @return {boolean}
+ */
+var arrayStringsAreEqual = function(word1, word2) {
+    const getString = (arr) => {
+        let str = "";
+        for(let i = 0; i < arr.length; i++){
+            str += arr[i];
+        }
+        return str;
+    }
+    
+    if(getString(word1) === getString(word2)){
+       return true;
+   }else {
+        return false;
+    }
+};
