7de77a3e49f4935f25c1a0c1f6935d409bfcedfb
ChangHyun Park <buschanny@gmail.com>
2023-05-11 13:34:50 +0900

    Time: 7 ms (20.29%), Space: 42.8 MB (9.40%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -4,15 +4,9 @@ class Solution {
         int len = word1.length() < word2.length() ? word2.length() : word1.length();
         
         for(int i = 0; i < len; i++){
-            if(word1.length() - 1 >= i){
-                str += word1.charAt(i);            
-            }
-            if(word2.length() - 1 >= i){
-                str += word2.charAt(i);            
-            }
+            if(word1.length() - 1 >= i) str += word1.charAt(i);            
+            if(word2.length() - 1 >= i) str += word2.charAt(i);            
         }
-        
-        System.out.println(str);
         return str;
     }
 }
