dbd2d69f30a2852196d2cd1d263bf7503cd6846c
ChangHyun Park <buschanny@gmail.com>
2023-07-16 23:38:34 +0900

    Time: 108 ms (26.47%), Space: 45.7 MB (64.71%) - LeetHub

--- 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.js
+++ 2745-construct-the-longest-new-string/2745-construct-the-longest-new-string.js
@@ -5,10 +5,7 @@
  * @return {number}
  */
 var longestString = function(x, y, z) {
-    if (x == y) {
-            return 4 * y + 2 * z;
-        }
-        
-        let min = Math.min(x, y);
-        return 4 * min + 2 + 2 * z;
+    if (x === y) return 4 * y + 2 * z;
+    let min = Math.min(x, y);
+    return 4 * min + 2 + 2 * z;
 };
