7a27173d6c0032cb112f69c1ddb662a5bf48682f
ChangHyun Park <buschanny@gmail.com>
2023-08-13 20:51:55 +0900

    Time: 92 ms (86.36%), Space: 51 MB (86.36%) - LeetHub

---
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var FindElements = function(root) {
+    this.set = new Set()
+    
+    let recover = (root, val) =>{
+        this.set.add(val);
+        if(root.left != null)   recover(root.left, val * 2 + 1)
+        if(root.right != null)  recover(root.right, val * 2 + 2)
+    }
+    
+    recover(root, 0)
+};
+
+/** 
+ * @param {number} target
+ * @return {boolean}
+ */
+FindElements.prototype.find = function(target) {
+    return this.set.has(target)
+};
