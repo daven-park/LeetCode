b61133c7d72b486631327e003ea79a4a73e6a137
ChangHyun Park <buschanny@gmail.com>
2023-08-20 22:06:44 +0900

    Time: 80 ms (53.67%), Space: 51.7 MB (33.11%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0, min = prices[0];
+        for(let i = 1; i < prices.length; i++)   {
+            min < prices[i] ?  max = Math.max(prices[i] - min, max) : min = prices[i];
+        }
+        return max;
+};
