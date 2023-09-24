8b759ea93d5a5685c1b1b1f2e7b4b50a6e597a42
ChangHyun Park <buschanny@gmail.com>
2023-09-24 22:20:09 +0900

    Time: 65 ms (57.92%), Space: 45.3 MB (53.85%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.js
+++ 1763-longest-nice-substring/1763-longest-nice-substring.js
@@ -5,22 +5,23 @@
 var longestNiceSubstring = function(s) {
     if(s.length < 2) return "";
     
-    const obj={};
+    const obj = {};
     
     for(let i = 0; i < s.length; i++) obj[s[i]]=1
     
     for(let i = 0; i < s.length; i++){
-        let element=s[i]
+        let element = s[i];
+        
         if(obj[element.toUpperCase()] && obj[element.toLowerCase()]) continue;
         
-        let left=s.substring(0,i);
-        let right=s.substring(i + 1);
+        let left = s.substring(0, i);
+        let right = s.substring(i + 1);
 
-        let ans1 = longestNiceSubstring(left)
-        let ans2 = longestNiceSubstring(right)
+        let ans1 = longestNiceSubstring(left);
+        let ans2 = longestNiceSubstring(right);
 
         
-        return ans1.length >= ans2.length ? ans1 : ans2
+        return ans1.length >= ans2.length ? ans1 : ans2;
     }
-    return s
+    return s;
 };
