a95f993b4c91bc0b2e1a51faa57874411faf6ee6
ChangHyun Park <buschanny@gmail.com>
2023-10-13 21:13:44 +0900

    Time: 56 ms (30.48%), Space: 42.6 MB (13.68%) - LeetHub

--- 0551-student-attendance-record-i/0551-student-attendance-record-i.js
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.js
@@ -3,13 +3,13 @@
  * @return {boolean}
  */
 var checkRecord = function(s) {
-    let temp = {};
-    let answer = true;
     if(s.includes("LLL")) return false;
-    for(let i = 0; i < s.length; i++){
-        temp[s[i]] = temp[s[i]] ? temp[s[i]] + 1 : 1;
-    }
-    if(temp['A'] >= 2) answer = false;
     
-    return answer;
+    let aCnt = 0;
+    for(let i = 0; i < s.length; i++){
+        if(s[i] == 'A') aCnt++;
+    }
+    if(aCnt >= 2) return false;
+    
+    return true;
 };
