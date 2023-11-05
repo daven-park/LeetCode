1a0cdc34cc1b0d3bcdc77e1aeb7c65863677757b
ChangHyun Park <buschanny@gmail.com>
2023-11-05 22:54:46 +0900

    Time: 58 ms (44.23%), Space: 41.7 MB (89.62%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
@@ -7,9 +7,7 @@ var timeRequiredToBuy = function(tickets, k) {
     let N = tickets.length;
 
     let i = 0;
-
     let count = 0;
-
     while (tickets[k] > 0) {
         let pos = i % N;
         if (tickets[pos]) {
