5100fc556d4b92e4c4f3ddc79924b5a53e427752
ChangHyun Park <buschanny@gmail.com>
2023-11-19 21:45:02 +0900

    Time: 327 ms (29.55%), Space: 43.8 MB (83.95%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canPartition = function(nums) {
+    let sum = nums.reduce((a, b) => a + b, 0);
+    
+    if (sum % 2 !== 0) return false;
+    
+    let half = sum / 2; 
+    let dp = [];
+    
+    dp[0] = true;
+    
+    for (let i = 0; i < nums.length; ++ i) {
+        let num = nums[i];
+        for (let j = half; j >= num; --j) dp[j] = dp[j] || dp[j - num];
+    }
+    
+    return dp[half] || false;
+};
