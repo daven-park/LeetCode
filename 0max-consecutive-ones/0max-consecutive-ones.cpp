ec412106650da5b871b5820353c489846b7f6ade
ChangHyun Park <buschanny@gmail.com>
2023-11-28 10:16:30 +0900

    Time: 42 ms, Memory: 36.5 MB - LeetHub

---
+++ 0max-consecutive-ones/0max-consecutive-ones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt = 0;
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == 0){
+                 res = max(res, cnt);
+                cnt = 0;
+                continue;
+            }
+            cnt++;
+        }
+        res = max(res, cnt);
+        
+        return res;
+    }
+};
