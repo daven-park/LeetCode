a5e6b6b9383c9f3107619fe834f2d1eb18a2d18f
ChangHyun Park <buschanny@gmail.com>
2023-12-03 21:52:46 +0900

    Time: 60 ms (92.16%), Space: 42.4 MB (68.30%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -7,22 +7,17 @@ var characterReplacement = function(s, k) {
     const map = {};
 	let max = 0; 
 	let maxCnt = 0;
-
 	let left = 0;
 	let right = 0;
 
 	while (right < s.length) {
 		const n = s[right];
-
 		map[n] = map[n] + 1 || 1;
 		maxCnt = Math.max(maxCnt, map[n]); 
-
-
 		while (right - left + 1 - maxCnt > k) {
 			map[s[left]]--; 
 			left++; 
 		}
-
 		max = Math.max(max, right - left + 1);
 		right++;
 	}
