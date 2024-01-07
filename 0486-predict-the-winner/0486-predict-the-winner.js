8fd4997671ad2a8110aeabc30d02cc1ff499f83c
ChangHyun Park <buschanny@gmail.com>
2024-01-07 23:28:15 +0900

    Time: 55 ms (79.41%), Space: 42.1 MB (59.56%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var predictTheWinner = function(nums) {
+    const memo = Array(nums.length).fill(0);
+    
+    for (let i = nums.length - 1; i >= 0; i--) {
+        for (let j = i; j < nums.length; j++) {
+            if (i === j)
+                memo[i] = nums[i];
+            else
+                memo[j] = Math.max(nums[i] - memo[j], nums[j] - memo[j - 1]);
+        }
+    }
+    return memo[nums.length - 1] >= 0;
+};
