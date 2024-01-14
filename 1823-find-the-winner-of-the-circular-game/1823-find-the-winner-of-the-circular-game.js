0c6e1c7b4a45cbacba951ac7173cca456d21ae51
ChangHyun Park <buschanny@gmail.com>
2024-01-14 23:25:55 +0900

    Time: 245 ms (8.82%), Space: 47.6 MB (23.53%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var findTheWinner = function(n, k) {
+    let q = [];
+    
+    for(let i = 1; i <= n;i++) q.push(i);
+
+    while(q.length > 1){
+        let cnt = k - 1;
+        
+        while(cnt > 0){
+            q.push(q.shift());
+            cnt--;
+        }
+        
+        q.shift();
+    }
+    
+    return q.shift();
+};
