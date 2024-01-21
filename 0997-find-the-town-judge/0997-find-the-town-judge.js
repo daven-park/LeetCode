8fcee544778ff3d819f71f77486934407c17a390
ChangHyun Park <buschanny@gmail.com>
2024-01-21 23:12:54 +0900

    Time: 90 ms (59.94%), Space: 57.6 MB (10.23%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.js
+++ 0997-find-the-town-judge/0997-find-the-town-judge.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var findJudge = function(n, trust) {
-    if(n==1) return 1;
+    if(n == 1) return 1;
   
     const arr = new Array(n + 1).fill(0);
     
     for([i, j] of trust){
-        arr[i] -= 1;
-        arr[j] += 1;
+        arr[i]--;
+        arr[j]++;
     }
     
-    for(let i = 0; i  < arr.length; i++){
+    for(let i = 0; i < arr.length; i++){
         if(arr[i] == n - 1) return i;
     }
     return -1;
