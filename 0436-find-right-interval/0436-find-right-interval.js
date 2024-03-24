c2acc8249e58e33eeeaf969a6899743e67490df2
ChangHyun Park <buschanny@gmail.com>
2024-03-25 00:38:37 +0900

    Time: 91 ms (67.68%), Space: 56.6 MB (86.87%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[]}
+ */
+var findRightInterval = function(intervals) {
+    const len = intervals.length;
+    const result = [];
+    const intervalsIdx = intervals.map((interval, index) => [interval[0], index]);
+    
+    intervalsIdx.sort((a, b) => a[0] - b[0]);
+    
+    for (let i = 0; i < len; i++) {
+        const currentIntervalEnd = intervals[i][1];
+        let left = 0;
+        let right = len - 1;
+        let target = -1;
+        
+        while (left <= right) {
+            const mid = Math.floor((left + right) / 2);
+            if (intervalsIdx[mid][0] >= currentIntervalEnd) {
+                target = intervalsIdx[mid][1];
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        result.push(target);
+    }
+    return result;
+};
