edf6353a1f2274feb3d4991585d976cc6d524c44
ChangHyun Park <buschanny@gmail.com>
2024-04-22 00:20:02 +0900

    Time: 84 ms (35.32%), Space: 68.8 MB (16.61%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+   
+    let num = [0];
+    
+    const traversal = (root, num) => {
+        if(!root) return;
+        else num[0] = num[0] + 1;
+    
+        traversal(root.left, num);
+        traversal(root.right, num);
+    }
+    traversal(root, num);
+    return num[0];
+};
