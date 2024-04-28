572ba6609b00008d1a1e9e20224d66f95073a24c
ChangHyun Park <buschanny@gmail.com>
2024-04-28 22:28:19 +0900

    Time: 59 ms (49.51%), Space: 50.3 MB (93.22%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.js
+++ 0841-keys-and-rooms/0841-keys-and-rooms.js
@@ -3,10 +3,10 @@
  * @return {boolean}
  */
 var canVisitAllRooms = function(rooms) {
-     const roomSet = new Set([0]);
+    const roomSet = new Set([0]);
 
     for (const room of roomSet) {
-      rooms[room].forEach(el => roomSet.add(el))
+        rooms[room].forEach(el => roomSet.add(el))
     }
 
     return roomSet.size === rooms.length;
