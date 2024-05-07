f954d007d8e8d7d0ba8d67529a102c4bdb164010
ChangHyun Park <buschanny@gmail.com>
2024-05-07 16:34:17 +0900

    Time: 78 ms (5.17%), Space: 48.9 MB (95.12%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    try{
+        return promise1.then((val) => promise2.then((val2) => val + val2))
+    } catch(error){
+        console.error(error);
+        throw error;
+    }
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
