789c4e9ab80093652468cfa03f652e33dbf7ec56
ChangHyun Park <buschanny@gmail.com>
2024-06-03 00:01:11 +0900

    Time: 59 ms (95.89%), Space: 59 MB (49.32%) - LeetHub

---
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var getWinner = function(arr, k) {
+    let ans = arr[0];
+    let wins = 0;
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (ans > arr[i]){
+            wins++;
+        }else {
+            wins = 1;
+            ans = arr[i];
+        }
+        
+        if (wins == k) break;
+    }
+    
+    return ans;
+};
