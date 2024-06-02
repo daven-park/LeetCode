40fd2a7b2ce02c086f4f905eac84850e5340b90a
ChangHyun Park <buschanny@gmail.com>
2024-06-03 00:01:47 +0900

    Time: 113 ms (17.98%), Space: 61.3 MB (43.26%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.js
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.js
@@ -4,13 +4,14 @@
  * @return {number}
  */
 var numberOfSubarrays = function(nums, k) {
-    var res = 0;
+    let res = 0;
     let odd = [];
     let last = 0;
+    
     for(let i = 0; i < nums.length; i++){
         if (nums[i] % 2 ===1) odd.push(i);
         if (odd.length > k) last = odd.shift() + 1;
         if (odd.length === k) res += odd[0] - last + 1;         
     }
-   return res;  
+    return res;  
 };
