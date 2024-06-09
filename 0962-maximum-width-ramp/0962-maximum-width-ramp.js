10acc53169de405f8c0b0c3fcd84e74a4331bc10
ChangHyun Park <buschanny@gmail.com>
2024-06-10 01:18:44 +0900

    Time: 68 ms (63.83%), Space: 54.1 MB (65.96%) - LeetHub

--- 0962-maximum-width-ramp/0962-maximum-width-ramp.js
+++ 0962-maximum-width-ramp/0962-maximum-width-ramp.js
@@ -3,13 +3,11 @@
  * @return {number}
  */
 var maxWidthRamp = function(nums) {
-     let stack = [0];
+    let stack = [0];
     let maxWidth = 0;
 
     for(let i = 1; i < nums.length - 1; i++){
-        if(nums[i] < nums[stack[stack.length - 1]]){
-            stack.push(i)
-        }
+        if(nums[i] < nums[stack[stack.length - 1]]) stack.push(i);
     }
     
     for(let j = nums.length - 1; j > 0; j--){
