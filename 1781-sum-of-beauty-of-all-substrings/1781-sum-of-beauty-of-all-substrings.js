66814a69f73786d31482d2e82770a5735e3e3947
ChangHyun Park <buschanny@gmail.com>
2024-06-24 00:04:37 +0900

    Time: 561 ms (39.80%), Space: 56.6 MB (18.45%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.js
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.js
@@ -3,20 +3,19 @@
  * @return {number}
  */
 var beautySum = function(s) {
-    let count = 0;
+    let cnt = 0;
     for (let i = 0; i < s.length; i++) {
-        let map = new Map()
-
+        let map = new Map();
         for (let j = i; j < s.length; j++) {
-            const current = s[j]
-            map.set(current, (map.get(current) || 0) + 1);
+            const cur = s[j]
+            map.set(cur, (map.get(cur) || 0) + 1);
 
             let arr = Array.from(map.values())
             max = Math.max(...arr);
             min = Math.min(...arr);
 
-            count += Math.max(0, max - min);
+            cnt += Math.max(0, max - min);
         }
     }
-    return count
+    return cnt
 };
