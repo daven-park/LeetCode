c53fc3a1f9f72299d429df87c50d0099495128da
ChangHyun Park <buschanny@gmail.com>
2024-06-30 22:39:45 +0900

    Time: 56 ms (46.43%), Space: 49.1 MB (53.57%) - LeetHub

--- 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.js
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.js
@@ -4,17 +4,14 @@
  * @return {string}
  */
 var digitSum = function(s, k) {
-    while (s.length > k) {
-        let temp = '';
-        for (let i = 0; i < s.length; i += k) {
-            let sub = s.slice(i, i + k);
-            let sum = 0;
-            for (const c of sub) sum += c - '0';
-
-            temp += sum;
+    while(s.length > k){
+        let x = 0;
+        let sum = [0];
+        for(let i = 0; i < s.length; i++){
+            if(i % k == 0 && i != 0) sum[++x] = 0;
+            sum[x] += parseInt(s[i]);
         }
-        s = temp;
+        s = sum.join("");
     }
-
     return s;
 };
