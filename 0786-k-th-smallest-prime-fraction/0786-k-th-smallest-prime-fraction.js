dd2ba72841c78128e3ca50a38497edcc211e72d6
ChangHyun Park <buschanny@gmail.com>
2024-07-05 17:35:41 +0900

    Time: 1608 ms (30.48%), Space: 126.7 MB (34.84%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number[]}
+ */
+var kthSmallestPrimeFraction = function(arr, k) {
+    let ans = [];
+    for(let i = 0; i < arr.length; i++){
+        for(let j = i + 1; j < arr.length; j++){
+            ans.push([arr[i], arr[j]]);
+        }
+    }
+    
+    ans.sort((a, b) => (a[0] / a[1]) - (b[0] / b[1]));
+    
+    return [ans[k - 1][0], ans[k - 1][1]]
+};
