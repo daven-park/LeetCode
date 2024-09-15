bcc37e8107ddba1580e2d755e472d16c781588d0
ChangHyun Park <buschanny@gmail.com>
2024-09-16 02:11:34 +0900

    Time: 64 ms (60.53%), Space: 52.1 MB (68.42%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.js
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var minFlipsMonoIncr = function(s) {
-    let minF = 0, counter = 0;
+    let minF = 0;
+    let cnt = 0;
+    
     for(let ch of s){
-        if(ch === '1') counter++;
+        if(ch === '1') cnt++;
         else minF++;
-        minF = Math.min(minF, counter);
+        minF = Math.min(minF, cnt);
     }
     return minF;
 };
