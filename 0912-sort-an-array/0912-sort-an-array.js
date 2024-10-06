93ec2fce88b8cec456b75e4ade79cd7c436de9ec
ChangHyun Park <buschanny@gmail.com>
2024-10-07 00:23:05 +0900

    Time: 1141 ms (7.99%), Space: 75.7 MB (19.18%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-  if (nums.length < 2) return nums;
+    if (nums.length < 2) return nums;
     let mid = Math.floor(nums.length/2);
     let left = nums.slice(0, mid);
     let right = nums.slice(mid);
