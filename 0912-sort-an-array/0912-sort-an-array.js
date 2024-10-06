29362f63ea153be06b9b1489f49c0e2bc621a3c8
ChangHyun Park <buschanny@gmail.com>
2024-10-07 00:20:45 +0900

    Time: 1133 ms (8.40%), Space: 76.5 MB (11.77%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArray = function(nums) {
+  if (nums.length < 2) return nums;
+    let mid = Math.floor(nums.length/2);
+    let left = nums.slice(0, mid);
+    let right = nums.slice(mid);
+    return merge(sortArray(left), sortArray(right));
+};
+
+const merge = (arr1, arr2) => {
+    let arr = [];
+    while (arr1.length && arr2.length) {
+        if (arr1[0] <= arr2[0]) arr.push(arr1.shift());
+        else arr.push(arr2.shift());
+    }
+    return [...arr, ...arr1, ...arr2];
+}
