67be04ff03cca455b5b13d123e7455504da36b27
ChangHyun Park <buschanny@gmail.com>
2024-11-17 20:52:28 +0900

    Time: 3 ms (13.72%), Space: 51.6 MB (5.92%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -4,6 +4,8 @@
  * @return {boolean}
  */
 var rotateString = function(s, goal) {
+    if(s.length !== goal.length) return false;
+    
     let arr = s.split("");
     for(let i = 0; i < s.length; i++){
         arr.push(arr.shift());
