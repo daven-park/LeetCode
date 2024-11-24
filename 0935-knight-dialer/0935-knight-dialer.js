e22ec32f82f359086b202950a6e3c88231818252
ChangHyun Park <buschanny@gmail.com>
2024-11-24 23:43:58 +0900

    Time: 166 ms (23.91%), Space: 55.8 MB (69.14%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.js
+++ 0935-knight-dialer/0935-knight-dialer.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var knightDialer = function(n) {
-    const MOD = 10**9 + 7;
+    const MOD = 10 ** 9 + 7;
     let map = [[4,6],[8,6],[7,9],[4,8],[0,3,9],[],[0,1,7],[2,6],[1,3],[2,4]];
     let cache = new Array(n).fill().map(() => new Array(10));
     const dfs = (idx, i) => {
