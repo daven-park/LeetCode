25c7def6e217c9dda32633a8cf8f4dd465b77db3
ChangHyun Park <buschanny@gmail.com>
2024-12-30 02:56:04 +0900

    Time: 6 ms (18.49%), Space: 52.7 MB (35.29%) - LeetHub

---
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let evenArr = [];
+    let len = nums.length;
+    for(let i = 0; i < len; i++){
+        let cur = nums.shift();
+        if(cur % 2 === 0){
+            evenArr.push(cur);
+        }else {
+            nums.push(cur);
+        }
+    }
+
+    return [...evenArr, ...nums];
+};
