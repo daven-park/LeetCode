06be0e1898312a0377f729d371606fc6a21d1cf9
ChangHyun Park <buschanny@gmail.com>
2025-01-28 10:50:11 +0900

    Time: 4 ms (61%), Space: 53.5 MB (16.61%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,15 +3,6 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let obj = {};
-    for(let i = 0; i <  nums.length; i++){
-        obj[nums[i]] = ++obj[nums[i]] || 1;
-    }
-    
-    for(const key in obj){
-        if(obj[key] > nums.length / 2){
-            return key;
-        }
-    }
-    return 0;
+    nums.sort();
+    return nums[parseInt(nums.length / 2)];
 };
