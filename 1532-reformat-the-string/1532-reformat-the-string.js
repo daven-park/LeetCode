234a0a86ac580f41aadb35c9a6cb5ea74d37a8c9
ChangHyun Park <buschanny@gmail.com>
2025-02-16 02:21:14 +0900

    Time: 7 ms (72.15%), Space: 53.9 MB (46.84%) - LeetHub

--- 1532-reformat-the-string/1532-reformat-the-string.js
+++ 1532-reformat-the-string/1532-reformat-the-string.js
@@ -9,14 +9,15 @@ var reformat = function(s) {
 
     max = s.match(/[0-9]/g) || [];
     min = s.match(/[a-z]/g) || [];
-    if (min.length > max.length) {
-        [min, max] = [max, min]
-    }
+
+    if (min.length > max.length) [min, max] = [max, min];
+    
     if (max.length - min.length <= 1) {
         for(let j = 0; j <= max.length; j++){
             if (max[j]) res += max[j];
             if (min[j]) res += min[j];
         }
     }
+    
     return res;
 };
